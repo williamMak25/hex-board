ed918f75d54add8e3ebd5a78695013072466d98b
waiyanwinlwin <waiyanwinlwin25@gmail.com>
2026-03-18 01:25:24 +0700

    feat:create comment apis

--- src/py/app/domain/comment/controller.py
+++ src/py/app/domain/comment/controller.py
@@ -1,8 +1,9 @@
 from uuid import UUID
 
-from litestar import Controller
-
+from litestar import Controller, post, get
+from litestar.exceptions import NotFoundException
 from app.domain.comment.service import CommentService
+from app.domain.comment.schemas import Comment, CreateComment
 from app.lib.deps import create_service_dependencies
 
 
@@ -23,3 +24,35 @@ class CommentController(Controller):
             "sort_order": "asc",
         },
     )
+
+    @post(operation_id="Create comment",path="/")
+    async def create_comment(
+        self,
+        comment_service: CommentService,
+        data: CreateComment,
+    )->Comment:
+        db_obj = await comment_service.create(data)
+        return comment_service.to_schema(db_obj,schema_type=Comment)
+
+
+    @get(operation_id="Get comment",path="/{comment_id:uuid}")
+    async def get_comment(
+        self,
+        comment_id:UUID,
+        comment_service:CommentService,
+    )->Comment:
+        db_obj = await comment_service.get_one_or_none(id = comment_id)
+        if db_obj is None:
+            raise NotFoundException(status_code=404,detail="Comment not found")
+        return comment_service.to_schema(db_obj,schema_type=Comment)
+
+
+    @get(operation_id="Get comments by card",path="/by-card/{card_id:uuid}")
+    async def get_comment_by_card(
+        self,
+        card_id:UUID,
+        comment_service:CommentService,
+    )->list[Comment]:
+        comments = await comment_service.list(card_id=card_id)
+
+        return [comment_service.to_schema(comment, schema_type=Comment) for comment in comments if len(comments) > 0]
--- src/py/app/domain/comment/schemas.py
+++ src/py/app/domain/comment/schemas.py
@@ -8,8 +8,9 @@ from app.lib.schema import CamelizedBaseStruct
 class Comment(CamelizedBaseStruct):
     message: str
     user_id: UUID
-    id: UUID
     card_id: UUID
+    
+    id: UUID
     created_at: datetime
     updated_at: datetime
     comment_user: User | None = None
